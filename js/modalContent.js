69095bc6afd63b92ae040f45e1966ebb633121af
sandeep-12 <sharmasandeep340@gmail.com>
2017-09-30 22:20:46 +0530

    Changes in dialog template

--- js/modalContent.js
+++ js/modalContent.js
@@ -1,8 +1,19 @@
-var dialogContent = '<dialog id="ys-dialog"><h4>ys-dialog</h4><br><p>press [esc] to close this dialog.</dialog>';
+var dialogHead = 'YS Survey';
+var dialogSummary = 'Please spare some time for us to understand you better.';
+var linksHref = ['https://www.qazqoz.esy.es'];
+//Should contain the same number of items as there in 'linksHref'
+var linksText2Display = ['Here'];
+var escMessage = 'Press ESC key to Close.';
+
+//The id='ya-dialog' and the var name 'dialogTemplate' should not be changed.
+var dialogTemplate = '<dialog id="ys-dialog"><strong>'+dialogHead+
+					 '</strong><br><p>'+dialogSummary+
+					 '</p><br><p>'+escMessage+
+					 '</p>';
 
 //Injecting the content
-var content = document.createElement('div');
+/*var content = document.createElement('div');
 content.innerHTML = dialogContent;
 document.body.appendChild(content);
-
+*/
 console.log("Modal Content.... END");
