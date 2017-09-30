791d9b4de1855691e7590f5e6843fe33e0665561
sandeep-12 <sharmasandeep340@gmail.com>
2017-09-30 20:23:18 +0530

    Injecting the content into the page.

--- js/modalContent.js
+++ js/modalContent.js
@@ -1,1 +1,6 @@
 var dialogContent = '<dialog id="ys-dialog"><h4>ys-dialog</h4><br><p>press [esc] to close this dialog.</dialog>';
+
+//Injecting the content
+var content = document.createElement('div');
+content.innerHTML = dialogContent;
+document.body.appendChild(content);
