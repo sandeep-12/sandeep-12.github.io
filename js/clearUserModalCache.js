52af9b1f47849b706af6cb7d69a31df05ddfb3c3
sandeep-12 <sharmasandeep340@gmail.com>
2017-10-01 13:15:01 +0530

    adding clear cache code

--- js/clearUserModalCache.js
+++ js/clearUserModalCache.js
@@ -1,5 +1,5 @@
 //tag to handle whether to show or not the dialog box
 var clearCacheEle = document.createElement('div');
 clearCacheEle.id = 'ys-dialog-hide';
-clearCacheEle.innerHTML = 'hide';
+clearCacheEle.name = 'hide';
 document.body.appendChild(clearCacheEle);
